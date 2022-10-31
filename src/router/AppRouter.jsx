1a1c1d2c579e6decc464837a9b525c334a88bd06
Atun <mali1996ua@gmail.com>
2022-10-31 20:51:39 +0300

    styling

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,6 +6,7 @@ import Home from "../../src/pages/home/Home";
 import Login from "../../src/pages/login/Login";
 import About from "../../src/pages/about/About";
 import Detail from "../../src/pages/detail/Detail";
+import PrivateRouter from "./PrivateRouter";
 const AppRouter = () => {
   return (
     <BrowserRouter>
@@ -14,8 +15,12 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
-        <Route path="about" element={<About />} />
-        <Route path="detail" element={<Detail />} />
+        <Route path="about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="detail" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
