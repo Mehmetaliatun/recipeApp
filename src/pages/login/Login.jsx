9635f261021f01e421ad3fc16e6dec0deca428d2
Atun <mali1996ua@gmail.com>
2022-10-31 21:26:09 +0300

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+import meal from "../../assets/meal.svg";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>{"<CW/>"}RECIPE</Header>
+        <StyledForm>
+          <StyledInput />
+          <StyledInput />
+          <StyledButton>LOGIN</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
