7d6f9485fecd4047842088f0fd99a84026887ae2
Atun <mali1996ua@gmail.com>
2022-11-01 20:26:20 +0300

    api add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const Home = () => {
+  const APP_ID = "a9ec6f9a";
+  const APP_KEY = "0380ecfba75a6e995a3ad48c1f15dca0";
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState("breakfast");
+  const [recipes, setRecipes] = useState("");
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+    const { data } = await axios(url);
+    console.log(data);
+    setRecipes(data.hits);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return <div>Home</div>;
 };
 
